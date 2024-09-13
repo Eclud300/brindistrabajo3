dd17398d45b7056a9eb7a3153694fb7e13c50671
cisco01 <cisco01@CISCO1>
2024-09-13 09:52:12 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n= int.Parse(Console.ReadLine());
+
+            imprimir(1, n);
+
+
+        }
+        static void imprimir(int actual, int n)
+        {
+            if (actual > n) return;
+            Console.WriteLine(actual);
+
+            imprimir(actual + 1, n);
+
+            Console.ReadKey();
+        }
+    }
+}
